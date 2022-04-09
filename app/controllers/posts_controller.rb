ae0e4b51e4c95381de1f9fcf6c51ea3431f3e8a7
A-Maruyama-dmm <com.mac.dm@gmail.com>
2022-04-09 09:07:52 +0000

    createの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,28 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @posts = Post.all
   end
 
   def edit
     @post = Post.find(params[:id])
   end
+  
+  def create
+   @post = Post.new(posts_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+    @posts = Post.all
+      render :index
+    end
+  end
+  
+  
+  
+  private
+  def posts_params
+    params.permit(:title, :content)
+  end
+  
 end
