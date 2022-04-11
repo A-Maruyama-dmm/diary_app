0579d363f31d0c345df87f375a2ea13b6336d250
A-Maruyama-dmm <com.mac.dm@gmail.com>
2022-04-11 07:03:03 +0000

    createの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,33 +1,37 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+  
   def index
     @posts = Post.all
     @post = Post.new
   end
 
+
   def show
     @post = Post.find(params[:id])
-    @posts = Post.all
   end
 
   def edit
     @post = Post.find(params[:id])
   end
-  
+
   def create
    @post = Post.new(posts_params)
     if @post.save
-      redirect_to post_path(@post)
+      redirect_to post_path(@posts)
     else
-    @posts = Post.all
+    @post = Post.all
       render :index
     end
   end
-  
-  
-  
+
+
+
   private
   def posts_params
     params.permit(:title, :content)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get '/' => 'home#top'
   get 'home/about' => 'home#about'
-  resources :posts
+  resources :posts, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+  end
   
 end
