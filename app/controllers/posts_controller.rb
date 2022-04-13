6d264ac0d13e66ae77c073b37ca98c4356c5f3be
A-Maruyama-dmm <com.mac.dm@gmail.com>
2022-04-13 06:05:57 +0000

    delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @posts = Post.all
   end
 
   def edit
@@ -25,6 +26,12 @@ class PostsController < ApplicationController
     @post = Post.all
       render :index
     end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+      redirect_to post_path
   end
 
 
